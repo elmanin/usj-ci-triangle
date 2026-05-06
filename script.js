501ce6c8b30c0a4a51716fe6791b7e61c542972c
elmanin <147382103+elmanin@users.noreply.github.com>
2026-05-06 16:16:12 +0000

    Introduce double quotes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var INVALID_ARGUMENTS = 'The arguments were not valid';
 var NOT_A_TRIANGLE = 'Impossible to create a triangle with those sides';
 var EQUILATERAL = 'Equilateral triangle';
 var ISOSCELES = 'Isosceles triangle';
-var SCALENE = 'Scalene triangle';
+var SCALENE = "Scalene triangle";
 
 /*
  * Given the three sides of a triangle calculates the type of the
